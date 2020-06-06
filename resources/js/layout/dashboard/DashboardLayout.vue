d50843329e350a755c793cb1bd9908111120281d
karim12345 <k.abdelmegeed@nouracademy.com>
2020-06-06 15:27:45 +0200

    add missing sidebar item

--- resources/js/layout/dashboard/DashboardLayout.vue
+++ resources/js/layout/dashboard/DashboardLayout.vue
@@ -3,6 +3,7 @@
     <side-bar>
       <template slot="links">
         <sidebar-link to="/accounts" :name="$t('sidebar.accounts')" icon="tim-icons icon-chart-pie-36"/>
+        <sidebar-link to="/balance" :name="$t('sidebar.balance')" icon="tim-icons icon-chart-pie-36"/>
       </template>
     </side-bar>
     <div class="main-panel">
